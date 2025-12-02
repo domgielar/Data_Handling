bd77fbe88cebaa928e2edca6e8e2ff0d72f8f5cf
Dominik Gielarowiec <dominikgielarowiec@gmail.com>
2025-12-02 14:36:16 -0500

    Intial commit merging 2 seperate files by manipulating the list first

--- merged.py
+++ merged.py
@@ -5,10 +5,16 @@ findex=pd.read_csv("MobileMoney.csv")
 
 years=["2011 [YR2011]","2014 [YR2014]","2017 [YR2017]","2021 [YR2021]"]
 
+series_simpler={
+    "mobileaccount.t.d": "Mobileaccount",
+    "account.t.d": "Account",
+    "g20.made": "DigitalPayment"
+}
+
 gdp_new = gdp.melt(id_vars=["Country Name"], value_vars=["2011 [YR2011]", "2014 [YR2014]", "2017 [YR2017]", "2021 [YR2021]", "2024 [YR2024]"],var_name="Year", value_name="GDP_growth")
 gdp_new["Year"] = gdp_new["Year"].str.slice(0,3).astype(int)
 
-findex_new = findex.melt(id_vars=["Country Name"],value_vars=["2011 [YR2011]", "2014 [YR2014]", "2017 [YR2017]", "2021 [YR2021]", "2024 [YR2024]"],var_name="Year", value_name="MobileMoney")
+findex_new = findex.melt(id_vars=["Country Name", "Series Code"],value_vars=["2011 [YR2011]", "2014 [YR2014]", "2017 [YR2017]", "2021 [YR2021]", "2024 [YR2024]"],var_name="Year", value_name="value")
 findex_new["Year"] = findex_new["Year"].str.slice(0,3).astype(int)
 
 
